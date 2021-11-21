b5359a7f87a323a61a52e6b28b7bc08d7a88f99a
Roni dos Santos <roninds14@gmail.com>
2021-11-21 10:20:58 -0300

    Melhrando a imprecao do tabuleiro

--- xadrez-front/Program.cs
+++ xadrez-front/Program.cs
@@ -15,11 +15,11 @@ namespace xadrez_front
 
                 tab = new Tabuleiro(8, 8);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 8));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 4));
 
                 Tela.imprimirTabuleiro(tab);                
             }catch(TabuleiroException e)
