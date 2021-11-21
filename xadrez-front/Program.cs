4fca424747f0b689ebdb4663f30daa82dc08e4a8
Roni dos Santos <roninds14@gmail.com>
2021-11-21 10:03:36 -0300

    Alteracao Calsses Peca, Tabuleiro, Rei e Torre. Criacao de TabuleiroException

--- xadrez-front/Program.cs
+++ xadrez-front/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using tabuleiro;
+using xadrez;
 
 namespace xadrez_front
 {
@@ -8,13 +9,28 @@ namespace xadrez_front
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab;
+            try
+            {
+                Tabuleiro tab;
 
-            tab = new Tabuleiro(8, 8);
+                tab = new Tabuleiro(8, 8);
 
-            Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 8));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
 
-            Console.ReadLine();
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+                Tela.imprimirTabuleiro(tab);                
+            }catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
         }
     }
 }
